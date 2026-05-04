97d06498ab4f1782629b080e5821c43a1cce5d15
KaiSchneider-Data <kai_schneider@freenet.de>
2026-05-04 21:30:07 +0200

    Delete sql/sql directory

--- sql/sql/03_retention.sql
+++
@@ -1,8 +0,0 @@
--- Step 1: First activity per user
-
-SELECT 
-    CustomerID,
-    MIN(DATE(InvoiceDate)) AS first_activity_date
-FROM online_retail
-WHERE CustomerID IS NOT NULL
-GROUP BY CustomerID;
