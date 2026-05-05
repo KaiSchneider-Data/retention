dcc5b6c3ff486e9d501781a29f2b6f3be67e78e0
KaiSchneider-Data <kai_schneider@freenet.de>
2026-05-05 17:27:24 +0200

    Update 03_retention.sql
    Day 1 retention check

--- sql/03_retention.sql
+++ sql/03_retention.sql
@@ -6,3 +6,21 @@ SELECT
 FROM online_retail
 WHERE CustomerID IS NOT NULL
 GROUP BY CustomerID;
+
+-- Step 2: Check if users return on Day 1
+
+SELECT 
+    a.CustomerID,
+    a.first_activity_date,
+    b.InvoiceDate
+FROM (
+    SELECT 
+        CustomerID,
+        MIN(DATE(InvoiceDate)) AS first_activity_date
+    FROM online_retail
+    WHERE CustomerID IS NOT NULL
+    GROUP BY CustomerID
+) a
+JOIN online_retail b
+    ON a.CustomerID = b.CustomerID
+WHERE DATE(b.InvoiceDate) = a.first_activity_date + INTERVAL '1 day';
